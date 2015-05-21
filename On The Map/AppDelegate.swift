893c15c8945db8bf52bea1accc0dd5bfb2725206
Luis Matute <luis.matute@me.com>
2015-05-21 14:28:20 -0600

    - Initial commit

---
+++ On The Map/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  On The Map
+//
+//  Created by Luis Matute on 5/21/15.
+//  Copyright (c) 2015 Luis Matute. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+
